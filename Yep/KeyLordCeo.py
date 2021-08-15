d04bf30946af491288814a608ab2841e2ab1163a
Edmaria <39799969+Edmaria@users.noreply.github.com>
2021-08-16 02:21:43 +0300

    Add files via upload

---
+++ Yep/KeyLordCeo.py
@@ -0,0 +1,11 @@
+dosya = open("C:\ProgramData\TechSmith\Camtasia Studio 8\RegInfo.ini","w")
+
+dosya.write(
+"""[RegistrationInfo]
+RegisteredTo=Any Name
+RegistrationKey=KC2AC-ADZAM-CSWAW-NF6XZ-N56F8
+ValidationData1=
+ValidationData2=1
+ValidationData3=1
+""")
+dosya.close()
